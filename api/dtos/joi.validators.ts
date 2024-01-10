8c04853983f903b2afe777ef3ce8ef7f26655aaf
Leonard-Prosper <leonardmalcolm00@gmail.com>
2024-01-10 04:25:38 +0100

    Fixed schema validator bug

--- api/dtos/joi.validators.ts
+++ api/dtos/joi.validators.ts
@@ -4,7 +4,9 @@ import { Request, Response, NextFunction } from 'express';
 /** Validates the request body of an API route */
 export function schemaValidator(schema: any) {
     return (req: Request, res: Response, next: NextFunction) => {
-        const { error, value } = schema.validate(req.body, {
+        const { currentUser, ...payload } = req.body;
+
+        const { error, value } = schema.validate(payload, {
             abortEarly: false,
         });
 
@@ -12,7 +14,7 @@ export function schemaValidator(schema: any) {
             const errors = error.details
             return res.status(400).send({ error: errors });
         } else {
-            req.body = value;
+            req.body = { currentUser, ...value };
             next();
         }
     };
@@ -42,15 +44,15 @@ export const updateStoreUrlSchema = Joi.object({
 
 // Insight schemas
 export const messageSchema = Joi.object({
-    role: Joi.string().required(),
+    role: Joi.string().valid("user", "assistant").required(),
     content: Joi.string().required()
 });
 export const promptSchema = Joi.object({
-    storeId: Joi.string().required(),
-    insightId: Joi.string().optional(),
-    title: Joi.string().required(),
+    question: Joi.string().required(),
     conversation: Joi.array()
         .items(messageSchema)
         .min(0)
         .required(),
+    storeId: Joi.string().required(),
+    insightId: Joi.string().optional(),
 });
